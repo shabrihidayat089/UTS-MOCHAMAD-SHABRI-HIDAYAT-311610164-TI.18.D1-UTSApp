3fed99e6119098422b6006c0f40cc4d0292668cb
shabrihidayat089 <40760233+shabrihidayat089@users.noreply.github.com>
2021-05-08 03:35:29 +0700

    Add files via upload
    UTS-MOCHAMAD SHABRI HIDAYAT-311610164-TI.18.D1-UTSApp

---
+++ UTS-SHABRI-311610164-SHABRI-master/app/src/main/java/com/example/uts_shabri_311610164_shabri/WebFragment.java
@@ -0,0 +1,33 @@
+package com.example.uts_shabri_311610164_shabri;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+
+public class WebFragment extends Fragment {
+
+    WebView webView;
+    WebSettings webSettings;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_web, container, false);
+
+        webView = view.findViewById(R.id.webView);
+        webSettings = webView.getSettings();
+
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl("https://www.google.com/");
+        return view;
+    }
+}
